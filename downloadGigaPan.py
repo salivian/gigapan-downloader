f1beae247624ed5907f1b8275ae5c2873a24f6ad
Javier Luengo <franciscojavierluengo@outlook.com>
2023-01-12 21:38:12 +0000

    Updated to python 3

--- downloadGigaPan.py
+++ downloadGigaPan.py
@@ -2,8 +2,7 @@
 # http://gigapan.org/gigapans/<photoid>>
 
 from xml.dom.minidom import *
-from urllib2 import *
-from urllib import *
+from urllib.request import *
 import sys,os,math
 
 def getText(nodelist):
@@ -38,28 +37,28 @@ h = urlopen(base+"/gigapans/%d.kml"%(photo_id))
 photo_kml=h.read()
 
 
-# find the width and height, level 
+# find the width and height, level
 dom = parseString(photo_kml)
 
 height=int(find_element_value(dom.documentElement, "maxHeight"))
 width=int(find_element_value(dom.documentElement, "maxWidth"))
 tile_size=int(find_element_value(dom.documentElement, "tileSize"))
 
-print width,height,tile_size
+print(width,height,tile_size)
 
 
 maxlevel = max(math.ceil(width/tile_size), math.ceil(height/tile_size))
 maxlevel = int(math.ceil(math.log(maxlevel)/math.log(2.0)))
 wt = int(math.ceil(width/tile_size))+1
 ht = int(math.ceil(height/tile_size))+1
-print wt,ht,maxlevel
+print(wt,ht,maxlevel)
 
 #loop around to get every tile
-for j in xrange(ht):
-    for i in xrange(wt):
+for j in range(ht):
+    for i in range(wt):
         filename = "%04d-%04d.jpg"%(i,j)
         url = "%s/get_ge_tile/%d/%d/%d/%d"%(base,photo_id, maxlevel,j,i)
-        print url, filename
+        print(url, filename)
         h = urlopen(url)
         fout = open(str(photo_id)+"/"+filename,"wb")
         fout.write(h.read())
